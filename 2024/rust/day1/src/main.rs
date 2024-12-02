f7c49ad5d5b4f70a5ad0fec6e81baee97ece1bad
Allyn L. Bottorff <allyn@b6f.net>
2024-12-02 15:03:41 -0600

    working day1 pt1

--- 2024/rust/day1/src/main.rs
+++ 2024/rust/day1/src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
-    println!("Hello, world!");
-
     let mut v1 = vec![3, 4, 2, 1, 3, 3];
     let mut v2 = vec![4, 3, 5, 3, 9, 3];
 
@@ -10,5 +8,24 @@ fn main() {
     let tuples = v1.iter().zip(v2.iter());
 
     let total_diffs: i32 = tuples.map(|x| i32::abs(x.0 - x.1)).sum();
-    println!("{}", total_diffs);
+    println!("sample data: {}", total_diffs);
+
+    let content = std::fs::read_to_string("data.txt").unwrap();
+    let lines = content.lines();
+
+    let mut v1: Vec<i32> = Vec::new();
+    let mut v2: Vec<i32> = Vec::new();
+    for line in lines {
+        let mut parts = line.split_whitespace();
+        v1.push(parts.next().unwrap().parse().unwrap());
+        v2.push(parts.next().unwrap().parse().unwrap());
+    }
+
+    v1.sort();
+    v2.sort();
+
+    let tuples = v1.iter().zip(v2.iter());
+
+    let total_diffs: i32 = tuples.map(|x| i32::abs(x.0 - x.1)).sum();
+    println!("test data: {}", total_diffs);
 }
