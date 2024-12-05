a728db44b37e49d296280a4cd9ee625034626a5f
Allyn L. Bottorff <allyn@b6f.net>
2024-12-05 08:47:03 -0600

    day3 pt2

--- 2024/rust/day3/src/main.rs
+++ 2024/rust/day3/src/main.rs
@@ -44,4 +44,53 @@ fn main() {
     let sum: i32 = pairs.iter().map(|p| p.0 * p.1).sum();
 
     println!("input data sum: {}", sum);
+
+    let test_input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    let input_with_newlines = test_input
+        .replace("do()", "\ndo()\n")
+        .replace("don't()", "\ndon't()\n");
+
+    for line in input_with_newlines.lines() {
+        println!("{}", line);
+    }
+
+    let input_with_newlines = contents
+        .replace("do()", "\ndo()\n")
+        .replace("don't()", "\ndon't()\n");
+
+    let mut enable = true;
+    let mut acc = 0;
+    for line in input_with_newlines.lines() {
+        if line == "do()" {
+            enable = true;
+            continue;
+        }
+        if line == "don't()" {
+            enable = false;
+            continue;
+        }
+        if enable == false {
+            continue;
+        }
+        if enable == true {
+            let mut fields: Vec<&str> = Vec::new();
+
+            for (_, [f1]) in re.captures_iter(line).map(|caps| caps.extract()) {
+                fields.push(f1);
+            }
+            let mut pairs: Vec<(i32, i32)> = Vec::new();
+
+            for field in fields {
+                let pair: Vec<&str> = field.split(',').collect();
+
+                let p1 = pair[0].parse::<i32>().unwrap();
+                let p2 = pair[1].parse::<i32>().unwrap();
+                pairs.push((p1, p2));
+            }
+
+            let sum: i32 = pairs.iter().map(|p| p.0 * p.1).sum();
+            acc += sum;
+        }
+    }
+    println!("pt 2 sum: {}", acc);
 }
