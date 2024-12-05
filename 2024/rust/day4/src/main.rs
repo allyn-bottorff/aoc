951cc9050aa99bc86386163cd80653656aee8a92
Allyn L. Bottorff <allyn@b6f.net>
2024-12-05 16:08:06 -0600

    day4 pt1

--- 2024/rust/day4/src/main.rs
+++ 2024/rust/day4/src/main.rs
@@ -11,7 +11,7 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
 
-    // let test_data = std::fs::read_to_string("data.txt").unwrap();
+    let test_data = std::fs::read_to_string("data.txt").unwrap();
 
     let word_search: Vec<Vec<char>> = test_data
         .lines()
@@ -20,10 +20,10 @@ MXMXAXMASX";
 
     let search_word = ['X', 'M', 'A', 'S'];
 
-    let i_max = word_search.len() - 1;
+    let i_len = word_search.len();
     let mut found: i32 = 0;
     for (i, line) in word_search.iter().enumerate() {
-        let j_max = line.len() - 1;
+        let j_len = line.len();
         for (j, _cell) in line.iter().enumerate() {
             //check up
             let mut local_search_word = search_word.iter();
@@ -44,7 +44,7 @@ MXMXAXMASX";
             //check upright
             let mut local_search_word = search_word.iter();
             for k in 0..search_word.len() {
-                if i >= search_word.len() - 1 && j < (j_max - search_word.len()) {
+                if i >= search_word.len() - 1 && j <= (j_len - search_word.len()) {
                     if let Some(letter) = local_search_word.next() {
                         if word_search[i - k][j + k] != *letter {
                             break;
@@ -60,7 +60,7 @@ MXMXAXMASX";
             //check right
             let mut local_search_word = search_word.iter();
             for k in 0..search_word.len() {
-                if j < j_max - search_word.len() {
+                if j <= j_len - search_word.len() {
                     if let Some(letter) = local_search_word.next() {
                         if word_search[i][j + k] != *letter {
                             break;
@@ -76,7 +76,7 @@ MXMXAXMASX";
             //check downright
             let mut local_search_word = search_word.iter();
             for k in 0..search_word.len() {
-                if (i < i_max - search_word.len()) && (j < j_max - search_word.len()) {
+                if (i <= i_len - search_word.len()) && (j <= j_len - search_word.len()) {
                     if let Some(letter) = local_search_word.next() {
                         if word_search[i + k][j + k] != *letter {
                             break;
@@ -92,7 +92,7 @@ MXMXAXMASX";
             //check down
             let mut local_search_word = search_word.iter();
             for k in 0..search_word.len() {
-                if i < i_max - search_word.len() {
+                if i <= i_len - search_word.len() {
                     if let Some(letter) = local_search_word.next() {
                         if word_search[i + k][j] != *letter {
                             break;
@@ -108,7 +108,7 @@ MXMXAXMASX";
             //check downleft
             let mut local_search_word = search_word.iter();
             for k in 0..search_word.len() {
-                if (i < i_max - search_word.len()) && (j >= search_word.len() - 1) {
+                if (i <= i_len - search_word.len()) && (j >= search_word.len() - 1) {
                     if let Some(letter) = local_search_word.next() {
                         if word_search[i + k][j - k] != *letter {
                             break;
